6163fd9cbd172e27c4f41c0cb3abdf014d663a23
Sayon Usman <sayonusman.ma@gmail.com>
2023-10-18 20:04:11 +0300

    Added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,265 @@
+import React from "react";
+import {
+  Navbar,
+  Collapse,
+  Typography,
+  IconButton,
+  Badge,
+  Tooltip,
+  Input,
+} from "@material-tailwind/react";
+import {
+  UserIcon,
+  HeartIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/solid";
+
+export default function StickyNavbar() {
+  const [openNav, setOpenNav] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 1140 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 xl:mb-0 xl:mt-0 xl:flex-row xl:items-center xl:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          All
+        </a>
+      </Typography>
+
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Gift Carts
+        </a>
+      </Typography>
+
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Track Order
+        </a>
+      </Typography>
+
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Returns
+        </a>
+      </Typography>
+
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Help Center
+        </a>
+      </Typography>
+    </ul>
+  );
+
+  return (
+    <div className="w-full shadow-md shadow-black">
+      <Navbar
+        variant="gradient"
+        color="deep-orange"
+        className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-4 xl:px-10 from-deep-orange-500 to-yellow-500"
+      >
+        <div className="flex items-center justify-between text-blue-gray-900">
+          <Typography
+            as="a"
+            href="#"
+            className="mr-4 cursor-pointer py-1.5 font-semibold"
+          >
+            Get By Click
+          </Typography>
+
+          <div className="flex items-center gap-1">
+            <div className="mr-4 hidden xl:block">{navList}</div>
+
+            <div className="hidden items-center gap-x-2 xl:flex xl:mr-5">
+              <form className="relative flex w-full gap-2 md:w-max">
+                <Input
+                  type="search"
+                  placeholder="Search"
+                  containerProps={{
+                    className: "min-w-[288px]",
+                  }}
+                  className="!border-blue-gray-900 pl-9 placeholder:text-blue-gray-900 text-blue-gray-900"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                />
+
+                <div className="!absolute left-3 top-[13px]">
+                  <svg
+                    width="13"
+                    height="14"
+                    viewBox="0 0 14 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9.97811 7.95252C10.2126 7.38634 10.3333 6.7795 10.3333 6.16667C10.3333 4.92899 9.84167 3.742 8.9665 2.86683C8.09133 1.99167 6.90434 1.5 5.66667 1.5C4.42899 1.5 3.242 1.99167 2.36683 2.86683C1.49167 3.742 1 4.92899 1 6.16667C1 6.7795 1.12071 7.38634 1.35523 7.95252C1.58975 8.51871 1.93349 9.03316 2.36683 9.4665C2.80018 9.89984 3.31462 10.2436 3.88081 10.4781C4.447 10.7126 5.05383 10.8333 5.66667 10.8333C6.2795 10.8333 6.88634 10.7126 7.45252 10.4781C8.01871 10.2436 8.53316 9.89984 8.9665 9.4665C9.39984 9.03316 9.74358 8.51871 9.97811 7.95252Z"
+                      fill="#000000"
+                    />
+
+                    <path
+                      d="M13 13.5L9 9.5M10.3333 6.16667C10.3333 6.7795 10.2126 7.38634 9.97811 7.95252C9.74358 8.51871 9.39984 9.03316 8.9665 9.4665C8.53316 9.89984 8.01871 10.2436 7.45252 10.4781C6.88634 10.7126 6.2795 10.8333 5.66667 10.8333C5.05383 10.8333 4.447 10.7126 3.88081 10.4781C3.31462 10.2436 2.80018 9.89984 2.36683 9.4665C1.93349 9.03316 1.58975 8.51871 1.35523 7.95252C1.12071 7.38634 1 6.7795 1 6.16667C1 4.92899 1.49167 3.742 2.36683 2.86683C3.242 1.99167 4.42899 1.5 5.66667 1.5C6.90434 1.5 8.09133 1.99167 8.9665 2.86683C9.84167 3.742 10.3333 4.92899 10.3333 6.16667Z"
+                      stroke="#000000"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+              </form>
+            </div>
+
+            <Tooltip
+              content="Log In"
+              placement="bottom-end"
+              className="rounded"
+            >
+              <IconButton color="amber" className="rounded mr-2 xl:mr-3">
+                <UserIcon className="h-full w-full" />
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip
+              content="Wishlist"
+              placement="bottom-end"
+              className="rounded"
+            >
+              <IconButton color="amber" className="rounded mr-2 xl:mr-3">
+                <HeartIcon className="h-full w-full" />
+              </IconButton>
+            </Tooltip>
+
+            <Badge
+              content="0"
+              color="white"
+              className="text-blue-gray-900 mr-3 xl:mr-0"
+            >
+              <Tooltip
+                content="Cart"
+                placement="bottom-end"
+                className="rounded"
+              >
+                <IconButton color="amber" className="rounded mr-3 xl:mr-0">
+                  <ShoppingCartIcon className="h-full w-full" />
+                </IconButton>
+              </Tooltip>
+            </Badge>
+
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent xl:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+
+        <Collapse open={openNav}>
+          {navList}
+
+          <div className="items-center gap-x-2 xl:hidden">
+            <form className="relative flex w-full gap-2 md:w-max">
+              <Input
+                type="search"
+                placeholder="Search"
+                containerProps={{
+                  className: "min-w-[288px]",
+                }}
+                className="!border-blue-gray-900 pl-9 placeholder:text-blue-gray-900 text-blue-gray-900"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+              />
+
+              <div className="!absolute left-3 top-[13px]">
+                <svg
+                  width="13"
+                  height="14"
+                  viewBox="0 0 14 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.97811 7.95252C10.2126 7.38634 10.3333 6.7795 10.3333 6.16667C10.3333 4.92899 9.84167 3.742 8.9665 2.86683C8.09133 1.99167 6.90434 1.5 5.66667 1.5C4.42899 1.5 3.242 1.99167 2.36683 2.86683C1.49167 3.742 1 4.92899 1 6.16667C1 6.7795 1.12071 7.38634 1.35523 7.95252C1.58975 8.51871 1.93349 9.03316 2.36683 9.4665C2.80018 9.89984 3.31462 10.2436 3.88081 10.4781C4.447 10.7126 5.05383 10.8333 5.66667 10.8333C6.2795 10.8333 6.88634 10.7126 7.45252 10.4781C8.01871 10.2436 8.53316 9.89984 8.9665 9.4665C9.39984 9.03316 9.74358 8.51871 9.97811 7.95252Z"
+                    fill="#000000"
+                  />
+
+                  <path
+                    d="M13 13.5L9 9.5M10.3333 6.16667C10.3333 6.7795 10.2126 7.38634 9.97811 7.95252C9.74358 8.51871 9.39984 9.03316 8.9665 9.4665C8.53316 9.89984 8.01871 10.2436 7.45252 10.4781C6.88634 10.7126 6.2795 10.8333 5.66667 10.8333C5.05383 10.8333 4.447 10.7126 3.88081 10.4781C3.31462 10.2436 2.80018 9.89984 2.36683 9.4665C1.93349 9.03316 1.58975 8.51871 1.35523 7.95252C1.12071 7.38634 1 6.7795 1 6.16667C1 4.92899 1.49167 3.742 2.36683 2.86683C3.242 1.99167 4.42899 1.5 5.66667 1.5C6.90434 1.5 8.09133 1.99167 8.9665 2.86683C9.84167 3.742 10.3333 4.92899 10.3333 6.16667Z"
+                    stroke="#000000"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+            </form>
+          </div>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import StickyNavbar from "./components/Navbar";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <h1>Get By Click</h1>
+      <StickyNavbar />
     </ThemeProvider>
   </React.StrictMode>
 );
