38820f7b18392a346005e998a1b7671287cddcbe
Sayon Usman <sayonusman.ma@gmail.com>
2023-11-12 04:50:52 +0300

    Added handleAddToCart function in item card and PCL's code has been updated

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -6,8 +6,100 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
+import { useState } from "react";
 
-export default function ItemCard({ collection }) {
+export default function ItemCard({
+  collection,
+  toast,
+  toastId,
+  newInfoToast,
+  newErrorToast,
+}) {
+  const [isAdded, setIsAdded] = useState();
+
+  const handleAddToCart = (event, item) => {
+    setIsAdded(!isAdded);
+
+    if (event.target.innerText === "ADD TO CART") {
+      const itemDetails = {
+        id: item._id,
+        title: item.title,
+        url: item.url,
+        price: item.price,
+        category: item.category,
+        subcategory: item.subcategory,
+      };
+
+      fetch("http://localhost:5000/selected-item", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(itemDetails),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data._id) {
+            if (toast.isActive(toastId.current)) {
+              toast.update(toastId.current, {
+                render: "Added To Cart",
+                type: "info",
+                autoClose: 5000,
+                hideProgressBar: false,
+                pauseOnHover: true,
+                closeOnClick: false,
+                draggable: false,
+              });
+            } else {
+              newInfoToast();
+            }
+          }
+        })
+        .catch((err) => {
+          toast.update(toastId.current, {
+            render: err.message,
+            type: "error",
+            autoClose: 5000,
+            hideProgressBar: false,
+            pauseOnHover: true,
+            closeOnClick: false,
+            draggable: false,
+          });
+        });
+    } else if (event.target.innerText === "REMOVE FROM CART") {
+      fetch(`http://localhost:5000/delete-item/${item._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.acknowledged) {
+            if (toast.isActive(toastId.current)) {
+              toast.update(toastId.current, {
+                render: "Removed From Cart",
+                type: "error",
+                autoClose: 5000,
+                hideProgressBar: false,
+                pauseOnHover: true,
+                closeOnClick: false,
+                draggable: false,
+              });
+            } else {
+              newErrorToast();
+            }
+          }
+        })
+        .catch((err) => {
+          toast.update(toastId.current, {
+            render: err.message,
+            type: "error",
+            autoClose: 5000,
+            hideProgressBar: false,
+            pauseOnHover: true,
+            closeOnClick: false,
+            draggable: false,
+          });
+        });
+    }
+  };
+
   return (
     <div className="flex justify-center items-center">
       <Card className="w-96 rounded-none bg-gradient-to-b from-blue-gray-100 to-white my-1 shadow-md shadow-blue-gray-700">
@@ -52,8 +144,13 @@ export default function ItemCard({ collection }) {
         </CardBody>
 
         <CardFooter className="pt-0">
-          <Button className="w-full rounded-none bg-white text-blue-gray">
-            Add to Cart
+          <Button
+            onClick={(event) => handleAddToCart(event, collection)}
+            className={`w-full rounded-none bg-white text-blue-gray ${
+              isAdded && "shadow-md shadow-deep-orange-500"
+            }`}
+          >
+            {isAdded ? "Remove from Cart" : "Add to Cart"}
           </Button>
         </CardFooter>
       </Card>
--- src/components/PrimaryCollectionsLayout.jsx
+++ src/components/PrimaryCollectionsLayout.jsx
@@ -1,12 +1,51 @@
+import { useRef } from "react";
 import ItemCard from "./ItemCard";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const PrimaryCollectionsLayout = ({ collections }) => {
+  const toastId = useRef(null);
+
+  const newInfoToast = () => {
+    toastId.current = toast.info("Added To Cart", {
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      draggable: false,
+    });
+  };
+
+  const newErrorToast = () => {
+    toastId.current = toast.error("Removed From Cart", {
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      draggable: false,
+    });
+  };
+
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 py-2">
-      {collections?.map((collection) => (
-        <ItemCard key={collection._id} collection={collection}></ItemCard>
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 py-2">
+        {collections?.map((collection) => (
+          <ItemCard
+            key={collection._id}
+            collection={collection}
+            toast={toast}
+            toastId={toastId}
+            newInfoToast={newInfoToast}
+            newErrorToast={newErrorToast}
+          ></ItemCard>
+        ))}
+      </div>
+
+      <ToastContainer
+        position="bottom-right"
+        newestOnTop={false}
+        rtl={false}
+        theme="colored"
+      />
+    </>
   );
 };
 
